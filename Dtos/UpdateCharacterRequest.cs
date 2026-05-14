1170423a1100235eea4541d1ba0e06c197be0e5a
olaholunuga <olaholunuga@gmail.com>
2026-05-14 14:58:14 +0100

    created: response DTOs

---
+++ Dtos/UpdateCharacterRequest.cs
@@ -0,0 +1,8 @@
+namespace NewApi.Dtos;
+
+public class UpdateCharacterRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Game { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
